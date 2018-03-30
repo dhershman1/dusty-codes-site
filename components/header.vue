946a391f48581e5423b823078394157daa99744a
dhershman1 <dustinh17@gmail.com>
2018-03-30 11:16:28 -0400

    Make it a little more known of which button is currently active

--- components/header.vue
+++ components/header.vue
@@ -1,9 +1,13 @@
 <template>
   <v-toolbar color="indigo" dark>
-    <v-toolbar-title>{{ title }} v{{ version }}</v-toolbar-title>
+    <v-toolbar-title class="mr-2">{{ title }} v{{ version }}</v-toolbar-title>
     <v-toolbar-items>
-      <v-btn flat @click="$emit('goInfo')"> <v-icon v-html="'info'"></v-icon> Info</v-btn>
-      <v-btn flat @click="$emit('goDocs')"> <v-icon v-html="'description'"></v-icon> Documentation</v-btn>
+      <v-btn flat :class="{active: current === 'info'}" @click="switchTo('info')">
+        <v-icon v-html="'info'"></v-icon> Info
+      </v-btn>
+      <v-btn flat :class="{active: current === 'docs'}" @click="switchTo('docs')">
+        <v-icon v-html="'description'"></v-icon> Documentation
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -19,6 +23,29 @@ export default {
       type: String,
       default: '0.0.0'
     }
+  },
+  data() {
+    return {
+      current: 'info'
+    };
+  },
+  methods: {
+    switchTo(to) {
+      this.current = to;
+
+      if (to === 'docs') {
+        this.$emit('goDocs');
+      } else {
+        this.$emit('goInfo');
+      }
+    }
   }
 };
 </script>
+
+<style scoped>
+.active {
+  background-color: hsla(0, 0%, 0%, 0.288) !important;
+};
+</style>
+
