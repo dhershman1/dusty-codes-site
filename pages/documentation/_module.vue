a8ac16527d0b46679b5c6abd6864a7a87546996a
dhershman1 <dustinh17@gmail.com>
2018-09-15 14:01:22 -0400

    Converted data call to actions for modules

--- pages/documentation/_module.vue
+++ pages/documentation/_module.vue
@@ -3,22 +3,22 @@
     <v-flex xs12>
       <v-layout align-center>
         <v-flex text-xs-center class="header-label">
-          <h3 class="display-3">{{ name | capitalize }} v{{ version }}</h3>
-          <badges :module="$route.params.module"></badges>
-          <h3>{{ description }}</h3>
+          <h3 class="display-3">{{ $route.params.module | capitalize }} v{{ module.version }}</h3>
+          <badges :module="$route.params.module" />
+          <h3>{{ module.description }}</h3>
         </v-flex>
       </v-layout>
       <v-tabs grow dark icons-and-text color="primary">
         <v-tabs-slider color="yellow"></v-tabs-slider>
-        <v-tab @click="currDisplay = 'readme'">
+        <v-tab @click="switchDisplay('readme')">
           Readme
           <v-icon>info</v-icon>
         </v-tab>
-        <v-tab v-if="changelog" @click="currDisplay = 'changelog'">
+        <v-tab v-if="changelogHtml" @click="switchDisplay('changelog')">
           Changelog
           <v-icon>assignment</v-icon>
         </v-tab>
-        <v-tab @click="currDisplay = 'methods'">
+        <v-tab @click="switchDisplay('methods')">
           Functions
           <v-icon>description</v-icon>
         </v-tab>
@@ -31,21 +31,19 @@
           :key="'readme'">
           <v-flex xs12>
             <v-card height="100%">
-              <v-card-text class="readme" v-html="currDisplay === 'readme' ? readMe : changelog" />
+              <v-card-text class="readme" v-html="currDisplay === 'readme' ? readmeHtml : changelogHtml" />
             </v-card>
           </v-flex>
         </v-layout>
-        <method-docs v-else :docs="docs" :key="'docs'" />
+        <method-docs v-else :key="'docs'" />
       </transition>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import axios from 'axios'
+import { mapGetters, mapState, mapMutations } from 'vuex'
 import capitalize from 'kyanite/capitalize'
-import marked from 'marked'
-import hljs from 'highlight.js'
 import badges from '../../components/badge'
 import methodDocs from '../../components/method-docs'
 
@@ -67,35 +65,19 @@ export default {
   filters: {
     capitalize
   },
-  asyncData ({ params }) {
-    const cl = params.module === 'kyanite' ? 'CHANGELOG' : 'changelog'
+  asyncData ({ params, store }) {
+    return store.dispatch('docs/fetchInfo', {
+      cl: params.module === 'kyanite' ? 'CHANGELOG' : 'changelog',
+      module: params.module
+    })
+  },
 
-    return Promise.all([
-      axios.get(`https://cdn.jsdelivr.net/npm/${params.module}@latest/README.md`),
-      axios.get(`https://cdn.jsdelivr.net/npm/${params.module}@latest/${cl}.md`),
-      axios.get(`https://cdn.jsdelivr.net/npm/${params.module}@latest/info.json`)
-    ]).then(([readme, changelog, info]) => {
-      const rmHtml = marked(readme.data, {
-        gfm: true,
-        langPrefix: 'hljs ',
-        highlight (code) {
-          return hljs.highlightAuto(code, ['javascript', 'html']).value
-        }
-      })
-      const clHtml = marked(changelog.data)
-
-      return Object.assign({ name: params.module }, info.data, {
-        currDisplay: 'readme',
-        readMe: rmHtml,
-        changelog: clHtml
-      })
-    }).catch(() => ({
-      name: params.module,
-      version: '0.0.0',
-      docs: [],
-      currDisplay: 'readme'
-    }))
-  }
+  computed: {
+    ...mapState(['currDisplay']),
+    ...mapState('docs', ['module']),
+    ...mapGetters('docs', ['readmeHtml', 'changelogHtml'])
+  },
+  methods: mapMutations(['switchDisplay'])
 }
 </script>
 
