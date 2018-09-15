42f9aa4f35a33c0ea6157be4f677d2b66170a157
dhershman1 <dustinh17@gmail.com>
2018-09-15 13:59:47 -0400

    Converted tousing the store for settings & docs

---
+++ store/docs.js
@@ -0,0 +1,88 @@
+import { get } from 'axios'
+import filter from 'kyanite/filter'
+import fuzzysearch from 'kyanite/fuzzySearch'
+import hljs from 'highlight.js'
+import marked from 'marked'
+import sortBy from 'kyanite/sortBy'
+import toLower from 'kyanite/toLower'
+import omit from 'kyanite/omit'
+
+export const state = () => ({
+  module: {},
+  selectedMethod: {},
+  readme: '',
+  changelog: '',
+  docs: []
+})
+
+export const getters = {
+  readmeHtml (state) {
+    return marked(state.readme, {
+      gfm: true,
+      langPrefix: 'hljs ',
+      highlight (code) {
+        return hljs.highlightAuto(code, ['javascript', 'html']).value
+      }
+    })
+  },
+
+  changelogHtml (state) {
+    return marked(state.changelog)
+  },
+
+  sortedDocs (state) {
+    return sortBy(x => x.title, state.docs)
+  },
+
+  filterDocs (state) {
+    return val =>
+      filter(({ title, category }) =>
+        fuzzysearch(toLower(val), toLower(title)) ||
+        fuzzysearch(toLower(val), toLower(category)), state)
+  }
+}
+
+export const mutations = {
+  setModule (state, data) {
+    state.module = data
+  },
+
+  setReadme (state, data) {
+    state.readme = data
+  },
+
+  setChangelog (state, data) {
+    state.changelog = data
+  },
+
+  setDocs (state, data) {
+    state.docs = data
+  },
+
+  selectMethod (state, data) {
+    state.selectedMethod = data
+  }
+}
+
+export const actions = {
+  fetchInfo ({ commit }, { module, cl }) {
+    Promise.all([
+      get(`https://cdn.jsdelivr.net/npm/${module}@latest/README.md`),
+      get(`https://cdn.jsdelivr.net/npm/${module}@latest/${cl}.md`),
+      get(`https://cdn.jsdelivr.net/npm/${module}@latest/info.json`)
+    ]).then(([readme, changelog, info]) => {
+      commit('setModule', omit('docs', info.data))
+      commit('setReadme', readme.data)
+      commit('setChangelog', changelog.data)
+      commit('setDocs', info.data.docs)
+      commit('selectMethod', info.data.docs[0])
+    }).catch(err => {
+      commit('setModule', {})
+      commit('setReadme', '')
+      commit('setChangelog', '')
+      commit('setDocs', [])
+
+      throw err
+    })
+  }
+}
