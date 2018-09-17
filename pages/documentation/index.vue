9d4b68c57c6e9141a9746ad7723add6beb6ec340
dhershman1 <dustinh17@gmail.com>
2018-09-17 10:44:23 -0400

    Removed buttons and made the cards clickabel on docs portal

--- pages/documentation/index.vue
+++ pages/documentation/index.vue
@@ -11,7 +11,7 @@
         md4
         v-for="(card, i) in cards"
         :key="i">
-        <v-card height="100%">
+        <v-card hover :to="card.destination" ripple height="100%">
           <v-toolbar color="primary" dark dense card>
             <v-toolbar-title>
               {{ card.title }}
@@ -24,11 +24,6 @@
               <p class="mt-2 card-desc">{{ card.description }}</p>
             </div>
           </v-card-title>
-          <v-card-actions>
-            <v-btn target="_blank" :href="card.github">View Github</v-btn>
-            <v-btn target="_blank" :href="card.changelog">View Changelog</v-btn>
-            <v-btn color="primary" dark :to="card.destination">Read Docs</v-btn>
-          </v-card-actions>
         </v-card>
       </v-flex>
     </v-layout>
@@ -46,32 +41,23 @@ export default {
     title: 'Documentation'
   },
   data: () => ({
-    gradient: 'to top right, rgba(63,81,181, .7), rgba(34,46,84, .7)',
     cards: [
       {
         title: 'Kyanite',
         description: 'A tiny library of single type pure functional operators and utility functions',
         destination: '/documentation/kyanite',
-        github: 'https://github.com/silicate-tools/kyanite',
-        changelog: 'https://github.com/silicate-tools/kyanite/blob/master/CHANGELOG.md'
       }, {
         title: 'Phone-Fns',
         description: 'A tiny library of utility functions based that make working with phone numbers easier',
         destination: '/documentation/phone-fns',
-        github: 'https://github.com/dhershman1/phone-fns',
-        changelog: 'https://github.com/dhershman1/phone-fns/blob/master/changelog.md'
       }, {
         title: 'Simply_Valid',
         description: 'A data driven validation library with built in functionality for easy checks to make sure data is valid before going anywhere',
         destination: '/documentation/simply_valid',
-        github: 'https://github.com/dhershman1/simply_valid',
-        changelog: 'https://github.com/dhershman1/simply_valid/blob/master/changelog.md'
       }, {
         title: 'Simple-Card',
         description: 'A credit card validation system that uses a luhn algorithm to strictly ensure the data passed in are valid card numbers, while also giving you extra bits of information to help make life a bit easier',
         destination: '/documentation/simple-card',
-        github: 'https://github.com/dhershman1/simple-card',
-        changelog: 'https://github.com/dhershman1/simple-card/blob/master/changelog.md'
       }
     ]
   })
